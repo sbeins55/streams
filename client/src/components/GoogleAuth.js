665eee09e66e55a297be78c1f2e01c8ceeb9a545
sbeins <sean.beins@gmail.com>
2019-03-26 17:03:20 -0500

    Adding redux forms

--- client/src/components/GoogleAuth.js
+++ client/src/components/GoogleAuth.js
@@ -19,7 +19,9 @@ class GoogleAuth extends React.Component {
   }
 
   onAuthChange = isSignedIn => {
-    isSignedIn ? this.props.signIn() : this.props.signOut();
+    isSignedIn
+      ? this.props.signIn(this.auth.currentUser.get().getId())
+      : this.props.signOut();
   };
 
   onAuthUpdate = () => {
